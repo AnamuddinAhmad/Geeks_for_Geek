2c408702a50301141ac502e74ca544469b4d59a8
Anamuddin Ahmad <anamuddinahmad0786@gmail.com>
2024-12-14 12:17:02 +0530

    Update Search_in_Rotated_Sorted_Array.java

--- Search_in_Rotated_Sorted_Array.java
+++ Search_in_Rotated_Sorted_Array.java
@@ -1,6 +1,9 @@
 class Solution {
     int search(int[] arr, int key) {
         // Complete this function
+         if(arr.length<1 && arr[0] != key){  // check the size of the array and if the 
+            return -1;                 // intial index it is not present then return -1.
+        }
         for(int i=0;i<arr.length;i++){
             if(arr[i]==key){
                 return i;
